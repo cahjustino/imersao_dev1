f33ade583f5e0be46f56f77caaccaefb5aabda7d
cahjustino <66473182+cahjustino@users.noreply.github.com>
2022-03-13 15:15:43 -0300

    Add files via upload

---
+++ calculkadora/calculadora-de-m-dia/dist/script.js
@@ -0,0 +1,15 @@
+var nome= "Guilherme"
+
+var n1=9
+var n2=7
+var n3=4
+var n4=2
+
+
+var notaFinal=(n1+n2+n3+n4) / 4
+
+var notaFixada=notaFinal.toFixed(1)
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+//isso é um comentario
